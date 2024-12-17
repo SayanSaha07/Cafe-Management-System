1ea861a4f69f355a075943cbf687c9d42c920fb7
SayanSaha07 <sahas8969@gmail.com>
2024-12-17 14:57:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Cafe-Management-System
+﻿## Tool and Languages
+
+> - XAMPP Control Panel
+> - HTML
+> - CSS
+> - JavaSript
+> - PHP
+> - MySQL
+
+
+# Non-Functional Requirements:
+
+> 1. Usability - Should be user friendly.
+> 2. Reliability - The system should be available to users for a certain time schedule.
+> 3. Scalability - How fast does the system return results and how much will this performance change with higher workloads.
+> 4. Maintainability - System should be easy to maintain and cost effective.
+> 5. Security - The system and its data must be protected against attack. \*\* confuse to implementation.
---
+++ components/footer.php
@@ -0,0 +1,105 @@
+ <footer class="footer">
+
+    <section class="grid">
+
+       <div class="box">
+
+          <h3>quick links</h3>
+
+          <a href="home.php"> Home </a>
+          <a href="menu.php"> Menu </a>
+          <a href="orders.php"> Order </a>
+          <a href="about.php"> About </a>
+       </div>
+
+       <div class="box">
+          <h3> Cafe Shop <i class="fas fa-shopping-basket"></i> </h3>
+          <div class="share">
+             <a href="#" class="fab fa-facebook-f"></a>
+             <a href="#" class="fab fa-twitter"></a>
+             <a href="https://www.instagram.com/sha_yawn_/?next=%2F" class="fab fa-instagram"></a>
+             <a href="#" class="fab fa-linkedin"></a>
+          </div>
+       </div>
+
+       <div class="box">
+
+          <h3>Opening Hours</h3>
+          <div class="dateinfo">
+             <p>MONDAY </p> <samp>9.00 - 22.00</samp>
+          </div>
+          <div class="dateinfo">
+             <p>TUESDAY </p> <samp>9.00 - 22.00</samp>
+          </div>
+          <div class="dateinfo">
+             <p>WEDNESDAY </p><samp>9.00 - 22.00</samp>
+          </div>
+          <div class="dateinfo">
+             <p>THURSDAY </p><samp>9.00 - 22.00</samp>
+          </div>
+          <div class="dateinfo">
+             <p>FRIDAY </p> <samp>9.00 - 22.00</samp>
+          </div>
+          <div class="dateinfo">
+             <p>SATURDAY </p> <samp>9.00 - 22.00</samp>
+          </div>
+          <div class="dateinfo">
+             <p>SUNDAY </p><samp>10.00 - 20.00</samp>
+          </div>
+
+
+       </div>
+
+       <div class="box">
+          <h3>contact info</h3>
+          <div class="phone">
+             <p> <i class="fas fa-phone"></i> +91 7005744572 </p>
+             <p> <i class="fas fa-phone"></i> +91 9774131981 </a>
+             <p> <i class="fas fa-envelope"></i> cafe.shop@gmail.com </p>
+          </div>
+
+          <div class="phone1">
+             <h3>Branch Location</h3>
+
+             <p> <i class="fas fa-map-marker-alt"></i>Sehore,Bhopal,Madhya Pradesh </p>
+             <p> <i class="fas fa-envelope"></i> cafe.shop@gmail.com </p>
+          </div>
+       </div>
+
+
+
+
+    </section>
+
+    
+
+ </footer>
+
+
+
+
+ <!-- footer section starts  
+
+<footer class="footer">
+
+   <div class="grid">
+
+      <div class="box1">
+         <h3> groco <i class="fas fa-shopping-basket"></i> </h3>
+         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, saepe.</p>
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+      </div>
+
+     
+     
+
+      
+
+</footer>
+
+footer section ends -->
